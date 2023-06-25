63c5c74348b3753e6372230e1cf0ebc737c324c3
softweredevloper <ssayam200@gmail.com>
2023-06-26 02:10:15 +0530

    account ui done

--- app/src/main/java/com/flaxstudio/wallpaperapp/fragments/AccountFragment.kt
+++ app/src/main/java/com/flaxstudio/wallpaperapp/fragments/AccountFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.flaxstudio.wallpaperapp.R
 import com.flaxstudio.wallpaperapp.databinding.FragmentAccountBinding
 
@@ -14,10 +15,19 @@ class AccountFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentAccountBinding.inflate(inflater , container , false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.liveWallpapper.setOnClickListener { Toast.makeText(requireContext(),"Clicked Live",Toast.LENGTH_LONG).show() }
+        binding.friend.setOnClickListener { Toast.makeText(requireContext(),"Clicked Friend",Toast.LENGTH_LONG).show() }
+        binding.feedback.setOnClickListener { Toast.makeText(requireContext(),"Clicked feedback",Toast.LENGTH_LONG).show() }
+        binding.helpCenter.setOnClickListener { Toast.makeText(requireContext(),"Clicked help",Toast.LENGTH_LONG).show() }
+        binding.about.setOnClickListener { Toast.makeText(requireContext(),"Clicked about",Toast.LENGTH_LONG).show() }
     }
 
 }
