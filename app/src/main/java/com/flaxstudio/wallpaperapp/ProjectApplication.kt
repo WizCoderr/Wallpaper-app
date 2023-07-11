5477f72299d63dd5d1e2c853620bb95c0b18fda2
softweredevloper <ssayam200@gmail.com>
2023-07-11 23:50:49 +0530

    kapt removed

--- app/src/main/java/com/flaxstudio/wallpaperapp/ProjectApplication.kt
+++ app/src/main/java/com/flaxstudio/wallpaperapp/ProjectApplication.kt
@@ -1,16 +1,15 @@
 package com.flaxstudio.wallpaperapp
 
 import android.app.Application
-import com.flaxstudio.wallpaperapp.source.db.WallpaperDatabase
-import com.flaxstudio.wallpaperapp.source.db.WallpaperRepository
 
 class ProjectApplication : Application() {
     // No need to cancel this scope as it'll be torn down with the process
 
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
-    private val database by lazy { WallpaperDatabase.getInstance(this) }
-    val projectRepository by lazy { WallpaperRepository(database.wallpaperDao()) }
+//    private val database by lazy { WallpaperDatabase.getInstance(this) }
+//    val wallpaperRepository by lazy { WallpaperRepository(database.wallpaperDao()) }
+//    val categoryRepository by lazy { CategoriesRepo(database.categoryDao()) }
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/flaxstudio/wallpaperapp/fragments/SearchFragment.kt
+++ app/src/main/java/com/flaxstudio/wallpaperapp/fragments/SearchFragment.kt
@@ -1,12 +1,11 @@
 package com.flaxstudio.wallpaperapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.flaxstudio.wallpaperapp.R
-import com.flaxstudio.wallpaperapp.databinding.FragmentAccountBinding
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.flaxstudio.wallpaperapp.databinding.FragmentSearchBinding
 
 
@@ -20,6 +19,11 @@ class SearchFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSearchBinding.inflate(inflater , container ,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.backBtn.setOnClickListener { findNavController().popBackStack() }
     }
 
 }
