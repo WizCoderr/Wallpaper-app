79decd3170af2cbc14ff9166802a8fff357102e8
softweredevloper <ssayam200@gmail.com>
2023-07-17 22:54:44 +0530

    SplashActivity added

---
+++ app/src/main/java/com/flaxstudio/wallpaperapp/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.flaxstudio.wallpaperapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+    }
+}
